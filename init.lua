592bdb257a14fb8c84a392f08b8ed005ce5487ea
Davi Silva <davisilvaphoto@gmail.com>
2024-01-20 15:52:09 -0300

    start dot file config

---
+++ init.lua
@@ -0,0 +1,8 @@
+require("davisilva.plugins-setup")
+require("davisilva.core.options")
+require("davisilva.core.keymaps")
+require("davisilva.core.colorscheme")
+require("davisilva.plugins.comment")
+require("davisilva.plugins.nvim-tree")
+
+-- https://www.youtube.com/watch?v=vdn_pKJUda8 32 minutes
